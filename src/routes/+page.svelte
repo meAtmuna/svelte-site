5ffcf5de116dafba274b78fd73af138c1d265e89
meAtmuna <me.anumta@gmail.com>
2026-04-30 17:01:39 +0500

    project setup and add simple code

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    type contact ={
+        name: string;
+        address: string;
+        phone: string;
+    };
+
+    let contacts: Contact[] = [
+        {name: 'Test', address: '123 Main St.', phone: '617-000-000' }
+    ];
+</script>
+
+<main class"white-block">
+    {#each contacts as contact}
+        <div class="contact-element hover:scale-105 transition-transform">
+        </div>
+</main>
